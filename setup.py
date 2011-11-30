1f924ada5d555e2a9481941356befc8b5dde480d
German Krauss <german@devartis.com>
2011-11-30 17:51:35 -0300

    Initial setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fabric_bricks',
+      version='0.1',
+      description='Fabric Bricks',
+      author='Germán Krauss',
+      author_email='german@devartis.com',
+      url='www.devartis.com',
+      packages=['fabric_bricks'],
+     )
