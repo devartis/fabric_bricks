c32c3ba758131d0c8dd15c203718980b259b93de
German Krauss <german@devartis.com>
2012-02-09 11:05:54 -0300

    New tasks added to git module

--- fabric_bricks/git/git.py
+++ fabric_bricks/git/git.py
@@ -6,8 +6,18 @@ from fabric_bricks.utils import execute
 @task
 def pull():
     """
-    Pulls latest changes from GIT
+    Pulls latest changes from Git
     """
     require('root', provided_by=('An environment task'))
     with cd(env.root):
         execute('git pull')
+
+
+@task
+def checkout(name):
+    """
+    Checkouts the given tag or branch
+    """
+    require('root', provided_by=('An environment task'))
+    with cd(env.root):
+        execute('git checkout %s' % name)
