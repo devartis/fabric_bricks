4c29cad2704356a65e4a3df1968b92eb058a45e0
Poli <poli@devartis.com>
2012-08-15 00:19:11 -0300

    Improved versioning

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
+#! coding: utf-8
 
 from distutils.core import setup
 
+version = __import__('fabric_bricks').__version__
+
 setup(name='fabric_bricks',
-      version='0.1',
+      version=version,
       description='Fabric Bricks',
       author='German Krauss',
       author_email='german@devartis.com',
-      url='www.devartis.com',
+      url='http://guthub.com/devartis/fabric_bricks',
       packages=['fabric_bricks', 'fabric_bricks.apache', 'fabric_bricks.git',
           'fabric_bricks.dev', 'fabric_bricks.django_tasks', 'fabric_bricks.mysql',
           'fabric_bricks.nginx', 'fabric_bricks.pip', 'fabric_bricks.project',
